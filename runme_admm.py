3a5d52d387b8a6f806742236393e033366fc8a0a
Christian Backs <christian.backs@gmx.de>
2021-09-11 15:43:05 +0200

    Adjust sweep_run.py

--- runme_admm.py
+++ runme_admm.py
@@ -53,7 +53,7 @@ if __name__ == '__main__':
     # select scenarios to be run
     scenarios = [
         scenario_base
-]
+    ]
 
     if args.admm:
 
--- sweep_run.py
+++ sweep_run.py
@@ -1,135 +1,159 @@
-﻿# TODO: the whole thing
-
-from multiprocessing import freeze_support
-from os.path import join
-import os
+﻿import argparse
+from datetime import date
+from multiprocessing import freeze_support, set_start_method
+from os import makedirs, mkdir
+from os.path import join, isdir
 import shutil
 
+import urbs
 from urbs import admm_async
+from urbs.input import read_input
 from urbs.runfunctions import prepare_result_directory
 from urbs.scenarios import scenario_base
-from urbs.admm_async import plot
-from urbs.admm_async import input_output
+from urbs.validation import validate_dc_objective, validate_input
 
-# input_files = 'mimo-example_internal.xlsx'  # for single year file name, for intertemporal folder name
-input_files = 'germany.xlsx'  # for single year file name, for intertemporal folder name
-input_dir = 'Input'
-input_path = os.path.join(input_dir, input_files)
-
-result_name = 'Run'
-result_dir = prepare_result_directory(result_name)  # name + time stamp
-
-# copy input file to result directory
-try:
-    shutil.copytree(input_path, os.path.join(result_dir, input_dir))
-except NotADirectoryError:
-    shutil.copyfile(input_path, os.path.join(result_dir, input_files))
-# copy run file to result directory
-shutil.copy(__file__, result_dir)
-
-# objective function
-objective = 'cost'  # set either 'cost' or 'CO2' as objective
-
-# simulation timesteps
-(offset, length) = (0, 10)  # time step selection
-timesteps = range(offset, offset + length + 1)
-dt = 1  # length of each time step (unit: hours)
-
-# clusters = [['Schleswig-Holstein','Hamburg','Mecklenburg-Vorpommern','Offshore','Lower Saxony','Bremen','Saxony-Anhalt','Brandenburg','Berlin','North Rhine-Westphalia'],
-#                ['Baden-Württemberg','Hesse','Bavaria','Rhineland-Palatinate','Saarland','Saxony','Thuringia']]
-# clusters = [
-#     ['Schleswig-Holstein', 'Hamburg', 'Mecklenburg-Vorpommern', 'Offshore'],
-#     ['Lower Saxony', 'Bremen', 'Saxony-Anhalt', 'Brandenburg'],
-#     ['Berlin', 'North Rhine-Westphalia', 'Baden-Württemberg', 'Hesse'],
-#     ['Bavaria', 'Rhineland-Palatinate', 'Saarland', 'Saxony', 'Thuringia']
-# ]
-clusters = [
-    ['Schleswig-Holstein'], ['Hamburg'], ['Mecklenburg-Vorpommern'], ['Offshore'],
-    ['Lower Saxony'], ['Bremen'], ['Saxony-Anhalt'], ['Brandenburg'],
-    ['Berlin'], ['North Rhine-Westphalia'], ['Baden-Württemberg'], ['Hesse'],
-    ['Bavaria'], ['Rhineland-Palatinate'], ['Saarland'], ['Saxony'], ['Thuringia']
-]
-
-# select scenarios to be run
-scenarios = [
-    scenario_base
-]
-
-rhos = {i: 10**i for i in range(-1, 1)}
-multiplier_values = [1 + 0.05 * i for i in range(1, 3)]
-mults = {f'{v:.2f}': v for v in multiplier_values}
-decreases_values = [0.5, 0.7, 0.9]
-decs = {f'{v:.2f}': v for v in decreases_values}
-
-print(f'sweeping over rhos: {list(rhos.values())}')
-print(f'sweeping over mults: {list(mults.values())}')
-print(f'sweeping over decs: {list(decs.values())}')
-
-line = input('Continue? (Y/n): ')
-if line not in ['', 'y']:
-    print('Exiting')
-    quit()
-
-admmopts = {
-    f'{rho_name}-{mult_name}-{dec_name}': admm_async.AdmmOption(
-        primal_tolerance = 0.01,
-        dual_tolerance = 0.01,
-        mismatch_tolerance = 0.01,
-        rho = rho,
-        max_penalty = 10**8,
-        penalty_mult = mult,
-        primal_decrease = dec,
-        max_iter = 100,
-        tolerance_mode = 'relative',
-    )
-    for rho_name, rho in rhos.items()
-    for mult_name, mult in mults.items()
-    for dec_name, dec in decs.items()
-}
 
 if __name__ == '__main__':
+    set_start_method("spawn")
     freeze_support()
-    for scenario in scenarios:
 
-        stats = []
+    options = argparse.ArgumentParser()
+    options.add_argument('-a', '--admm', action='store_true')
+    options.add_argument('-s', '--sequential', action='store_true')
+    args = options.parse_args()
 
-        data_all, ttime = admm_async.read(input_path, scenario, objective)
+    input_files = 'germany.xlsx'  # for single year file name, for intertemporal folder name
+    input_dir = 'Input'
+    input_path = join(input_dir, input_files)
 
-        centralized_result = admm_async.run_centralized(
-            data_all, timesteps, dt, scenario, result_dir
-        )
+    result_name = 'germany'
+    if args.admm:
+        result_name += '-admm-fixed'
+    if args.sequential:
+        result_name += '-seq'
+    result_dir = prepare_result_directory(result_name)  # name + time stamp
 
-        for name, admmopt in admmopts.items():
-            print(f'Starting run {name}')
-            sub_dir = os.path.join(result_dir, name)
-            os.mkdir(sub_dir)
+    # copy input file to result directory
+    try:
+        shutil.copytree(input_path, join(result_dir, input_dir))
+    except NotADirectoryError:
+        shutil.copyfile(input_path, join(result_dir, input_files))
 
-            admm_results = admm_async.run_parallel(
-                data_all = data_all,
-                timesteps = timesteps,
-                scenario = scenario.__name__,
-                result_dir = sub_dir,
-                dt = dt,
-                objective = objective,
-                clusters = clusters,
-                admmopt = admmopt,
+    # copy run file to result directory
+    shutil.copy(__file__, result_dir)
+
+    # objective function
+    objective = 'cost'  # set either 'cost' or 'CO2' as objective
+
+    # simulation timesteps
+    (offset, length) = (0, 1)  # time step selection
+    timesteps = range(offset, offset + length + 1)
+    dt = 1  # length of each time step (unit: hours)
+
+    # select scenarios to be run
+    scenarios = [
+        scenario_base
+    ]
+
+    if args.admm:
+
+        # one cluster
+        # clusters = [['Schleswig-Holstein','Hamburg','Mecklenburg-Vorpommern','Offshore','Lower Saxony','Bremen','Saxony-Anhalt','Brandenburg','Berlin','North Rhine-Westphalia'],
+        #                ['Baden-Württemberg','Hesse','Bavaria','Rhineland-Palatinate','Saarland','Saxony','Thuringia']]
+
+        # four clusters
+        # clusters = [
+        #     ['Schleswig-Holstein', 'Hamburg', 'Mecklenburg-Vorpommern', 'Offshore'],
+        #     ['Lower Saxony', 'Bremen', 'Saxony-Anhalt', 'Brandenburg'],
+        #     ['Berlin', 'North Rhine-Westphalia', 'Baden-Württemberg', 'Hesse'],
+        #     ['Bavaria', 'Rhineland-Palatinate', 'Saarland', 'Saxony', 'Thuringia']
+        # ]
+
+        # one cluster per state
+        clusters = [
+            ['Schleswig-Holstein'], ['Hamburg'], ['Mecklenburg-Vorpommern'], ['Offshore'],
+            ['Lower Saxony'], ['Bremen'], ['Saxony-Anhalt'], ['Brandenburg'],
+            ['Berlin'], ['North Rhine-Westphalia'], ['Baden-Württemberg'], ['Hesse'],
+            ['Bavaria'], ['Rhineland-Palatinate'], ['Saarland'], ['Saxony'], ['Thuringia']
+        ]
+
+        admmopts = {
+            f'10^{i}': admm_async.AdmmOption(
+                tolerance_mode = 'relative',
+                primal_tolerance = 0.01,
+                dual_tolerance = 0.01,
+                mismatch_tolerance = 0.01,
+                penalty_mode='fixed',
+                rho = 10**i,
+                max_iter = 200,
             )
+            for i in range(4)
+        }
 
-            obj_cent = centralized_result['objective']
-            obj_admm = admm_results['admm_objective']
-            gap = (obj_admm - obj_cent) / obj_cent
-            admm_results['centralized_objective'] = obj_cent
-            admm_results['objective_gap'] = gap
-            admm_results['centralized_time'] = centralized_result['time']
+        for scenario in scenarios:
+            for opt_name, admmopt in admmopts.items():
+                scenario_dir = join(result_dir, scenario.__name__, opt_name)
+                makedirs(scenario_dir)
 
-            stats.append({
-                'name': name,
-                'time': admm_results['admm_time'],
-                'gap': 100 * abs(gap),
-            })
+                year = date.today().year
+                data_all = read_input(input_path, year)
 
-            input_output.save_results(admm_results, sub_dir)
-            plot.plot_results(admm_results, sub_dir, plot_rho=True)
-            shutil.copy(join(sub_dir, 'combined.svg'), join(result_dir, f'{name}.svg'))
+                data_all, cross_scenario_data = scenario(data_all, cross_scenario_data)
+                validate_input(data_all)
+                validate_dc_objective(data_all, objective)
 
-        input_output.save_overview(stats, result_dir)
+                if args.sequential:
+                    admm_async.run_sequential(
+                        'gurobi',
+                        data_all,
+                        timesteps,
+                        scenario_dir,
+                        dt,
+                        objective,
+                        clusters,
+                        admmopt,
+                        microgrid_cluster_mode='microgrid',
+                        cross_scenario_data=cross_scenario_data,
+                    )
+                else:
+                    admm_objective = admm_async.run_parallel(
+                    data_all,
+                    timesteps,
+                    scenario_dir,
+                    dt,
+                    objective,
+                    clusters,
+                    admmopt,
+                    microgrid_cluster_mode='microgrid',
+                    cross_scenario_data=cross_scenario_data,
+                )
+
+    else:
+        solver = 'gurobi'
+        threads = 1
+
+        report_tuples = []
+        report_sites_name = {}
+        plot_tuples = []
+        plot_sites_name = {}
+        plot_periods = {
+            'all': timesteps[1:]
+        }
+
+        for scenario in scenarios:
+            prob, cross_scenario_data = urbs.run_scenario(
+                input_path,
+                solver,
+                timesteps,
+                scenario,
+                result_dir,
+                dt,
+                objective,
+                cross_scenario_data=cross_scenario_data,
+                threads=threads,
+                report_tuples=report_tuples,
+                report_sites_name=report_sites_name,
+                plot_tuples=plot_tuples,
+                plot_sites_name=plot_sites_name,
+                plot_periods=plot_periods,
+            )
