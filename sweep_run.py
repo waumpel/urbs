50603b5fd5790a0b0beac080565bcdfa4e133236
Christian Backs <christian.backs@gmx.de>
2021-09-21 20:14:25 +0200

    Update sweep_run.py

--- sweep_run.py
+++ sweep_run.py
@@ -1,11 +1,10 @@
 ﻿import argparse
 from datetime import date
 from multiprocessing import freeze_support, set_start_method
-from os import makedirs, mkdir
-from os.path import join, isdir
+from os import makedirs
+from os.path import join
 import shutil
 
-import urbs
 from urbs import admm_async
 from urbs.input import read_input
 from urbs.runfunctions import prepare_result_directory
@@ -18,7 +17,6 @@ if __name__ == '__main__':
     freeze_support()
 
     options = argparse.ArgumentParser()
-    options.add_argument('-a', '--admm', action='store_true')
     options.add_argument('-s', '--sequential', action='store_true')
     args = options.parse_args()
 
@@ -26,11 +24,10 @@ if __name__ == '__main__':
     input_dir = 'Input'
     input_path = join(input_dir, input_files)
 
-    result_name = 'germany'
-    if args.admm:
-        result_name += '-admm-fixed'
+    result_name = 'germany-t8760-admm'
     if args.sequential:
         result_name += '-seq'
+    result_name += '-inc'
     result_dir = prepare_result_directory(result_name)  # name + time stamp
 
     # copy input file to result directory
@@ -46,7 +43,7 @@ if __name__ == '__main__':
     objective = 'cost'  # set either 'cost' or 'CO2' as objective
 
     # simulation timesteps
-    (offset, length) = (0, 1)  # time step selection
+    (offset, length) = (0, 8760)  # time step selection
     timesteps = range(offset, offset + length + 1)
     dt = 1  # length of each time step (unit: hours)
 
@@ -55,68 +52,59 @@ if __name__ == '__main__':
         scenario_base
     ]
 
-    if args.admm:
+    # one cluster
+    # clusters = [['Schleswig-Holstein','Hamburg','Mecklenburg-Vorpommern','Offshore','Lower Saxony','Bremen','Saxony-Anhalt','Brandenburg','Berlin','North Rhine-Westphalia'],
+    #                ['Baden-Württemberg','Hesse','Bavaria','Rhineland-Palatinate','Saarland','Saxony','Thuringia']]
 
-        # one cluster
-        # clusters = [['Schleswig-Holstein','Hamburg','Mecklenburg-Vorpommern','Offshore','Lower Saxony','Bremen','Saxony-Anhalt','Brandenburg','Berlin','North Rhine-Westphalia'],
-        #                ['Baden-Württemberg','Hesse','Bavaria','Rhineland-Palatinate','Saarland','Saxony','Thuringia']]
+    # four clusters
+    # clusters = [
+    #     ['Schleswig-Holstein', 'Hamburg', 'Mecklenburg-Vorpommern', 'Offshore'],
+    #     ['Lower Saxony', 'Bremen', 'Saxony-Anhalt', 'Brandenburg'],
+    #     ['Berlin', 'North Rhine-Westphalia', 'Baden-Württemberg', 'Hesse'],
+    #     ['Bavaria', 'Rhineland-Palatinate', 'Saarland', 'Saxony', 'Thuringia']
+    # ]
 
-        # four clusters
-        # clusters = [
-        #     ['Schleswig-Holstein', 'Hamburg', 'Mecklenburg-Vorpommern', 'Offshore'],
-        #     ['Lower Saxony', 'Bremen', 'Saxony-Anhalt', 'Brandenburg'],
-        #     ['Berlin', 'North Rhine-Westphalia', 'Baden-Württemberg', 'Hesse'],
-        #     ['Bavaria', 'Rhineland-Palatinate', 'Saarland', 'Saxony', 'Thuringia']
-        # ]
+    # one cluster per state
+    clusters = [
+        ['Schleswig-Holstein'], ['Hamburg'], ['Mecklenburg-Vorpommern'], ['Offshore'],
+        ['Lower Saxony'], ['Bremen'], ['Saxony-Anhalt'], ['Brandenburg'],
+        ['Berlin'], ['North Rhine-Westphalia'], ['Baden-Württemberg'], ['Hesse'],
+        ['Bavaria'], ['Rhineland-Palatinate'], ['Saarland'], ['Saxony'], ['Thuringia']
+    ]
 
-        # one cluster per state
-        clusters = [
-            ['Schleswig-Holstein'], ['Hamburg'], ['Mecklenburg-Vorpommern'], ['Offshore'],
-            ['Lower Saxony'], ['Bremen'], ['Saxony-Anhalt'], ['Brandenburg'],
-            ['Berlin'], ['North Rhine-Westphalia'], ['Baden-Württemberg'], ['Hesse'],
-            ['Bavaria'], ['Rhineland-Palatinate'], ['Saarland'], ['Saxony'], ['Thuringia']
-        ]
+    admmopts = {
+        f'rho={rho}-mult={mult}-dec={dec}': admm_async.AdmmOption(
+            tolerance_mode = 'relative',
+            primal_tolerance = 0.01,
+            dual_tolerance = 0.01,
+            mismatch_tolerance = 0.01,
+            penalty_mode='increasing',
+            rho = rho,
+            max_penalty=10**8,
+            penalty_mult=mult,
+            primal_decrease=dec,
+            max_iter = 200,
+        )
+        for rho in [0.01]
+        for mult in [1.15]
+        for dec in [0.95]
+    }
 
-        admmopts = {
-            f'10^{i}': admm_async.AdmmOption(
-                tolerance_mode = 'relative',
-                primal_tolerance = 0.01,
-                dual_tolerance = 0.01,
-                mismatch_tolerance = 0.01,
-                penalty_mode='fixed',
-                rho = 10**i,
-                max_iter = 200,
-            )
-            for i in range(4)
-        }
+    for scenario in scenarios:
+        for opt_name, admmopt in admmopts.items():
+            scenario_dir = join(result_dir, scenario.__name__, opt_name)
+            makedirs(scenario_dir)
 
-        for scenario in scenarios:
-            for opt_name, admmopt in admmopts.items():
-                scenario_dir = join(result_dir, scenario.__name__, opt_name)
-                makedirs(scenario_dir)
+            year = date.today().year
+            data_all = read_input(input_path, year)
 
-                year = date.today().year
-                data_all = read_input(input_path, year)
+            data_all, _ = scenario(data_all)
+            validate_input(data_all)
+            validate_dc_objective(data_all, objective)
 
-                data_all, cross_scenario_data = scenario(data_all, cross_scenario_data)
-                validate_input(data_all)
-                validate_dc_objective(data_all, objective)
-
-                if args.sequential:
-                    admm_async.run_sequential(
-                        'gurobi',
-                        data_all,
-                        timesteps,
-                        scenario_dir,
-                        dt,
-                        objective,
-                        clusters,
-                        admmopt,
-                        microgrid_cluster_mode='microgrid',
-                        cross_scenario_data=cross_scenario_data,
-                    )
-                else:
-                    admm_objective = admm_async.run_parallel(
+            if args.sequential:
+                admm_async.run_sequential(
+                    'gurobi',
                     data_all,
                     timesteps,
                     scenario_dir,
@@ -125,35 +113,15 @@ if __name__ == '__main__':
                     clusters,
                     admmopt,
                     microgrid_cluster_mode='microgrid',
-                    cross_scenario_data=cross_scenario_data,
                 )
-
-    else:
-        solver = 'gurobi'
-        threads = 1
-
-        report_tuples = []
-        report_sites_name = {}
-        plot_tuples = []
-        plot_sites_name = {}
-        plot_periods = {
-            'all': timesteps[1:]
-        }
-
-        for scenario in scenarios:
-            prob, cross_scenario_data = urbs.run_scenario(
-                input_path,
-                solver,
+            else:
+                admm_objective = admm_async.run_parallel(
+                data_all,
                 timesteps,
-                scenario,
-                result_dir,
+                scenario_dir,
                 dt,
                 objective,
-                cross_scenario_data=cross_scenario_data,
-                threads=threads,
-                report_tuples=report_tuples,
-                report_sites_name=report_sites_name,
-                plot_tuples=plot_tuples,
-                plot_sites_name=plot_sites_name,
-                plot_periods=plot_periods,
+                clusters,
+                admmopt,
+                microgrid_cluster_mode='microgrid',
             )
